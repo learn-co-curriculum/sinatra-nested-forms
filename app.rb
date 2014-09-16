d2cf0223f99b36b187597eeb55bbaef64a802bc5
Ian Miller <irmiller22@gmail.com>
2014-09-16 18:57:36 -0400

    controller/route logic implemented in app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,61 @@
 require './environment'
 
+class Pirate
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+end
+
+class Ship
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+end
+
 module FormsLab
   class App < Sinatra::Base
-    # define your routes here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+
+      @pirate = Pirate.new(params[:pirate])
+
+      binding.pry
+      params[:ship].each do |ship, details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
   end
 end
