f4e433b8d55a106a000a06d3429290fc72d4838e
Ian Miller <irmiller22@gmail.com>
2014-09-16 17:56:16 -0400

    set up environment configurations

--- environment.rb
+++ environment.rb
@@ -1,5 +1,7 @@
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler'
-Bundler.require(:default)
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
 require 'sinatra/base'
 require 'sinatra/reloader'
