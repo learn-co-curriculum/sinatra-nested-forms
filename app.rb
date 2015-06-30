89f95154ac4514d42670e1eb16f1e66143a8f076
Rose Weixel <rose.weixel@gmail.com>
2015-06-30 11:44:57 -0400

    nest ships under pirate in form (with solution)

--- app.rb
+++ app.rb
@@ -4,6 +4,18 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :'root'
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = params[:pirate]
+      erb :'pirates/show'
+    end
 
   end
 end
