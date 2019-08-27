029d22e7aced2618aaf370baeb641b9033a399fc
Flatiron Student <flatironstudent@Flatirons-MacBook-Air-6.local>
2019-08-27 16:08:28 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
 class Ship
+    
+    attr_accessor :name, :type, :booty
+
+    @@ships = []
+
+    def initialize (args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@ships << self
+    end
+
+    def self.all
+        @@ships 
+    end
+
+    def self.clear
+        @@ships = []
+    end
+
+
 end
