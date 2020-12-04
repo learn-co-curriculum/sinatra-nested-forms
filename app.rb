a50e27ad89d24cd7a5d05f912a35654014e1ca29
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-12-04 14:45:44 -0500

    commit tree 643cd27837674b285c62bfb924fe6a6982ffcac2

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+    
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
+
 class Ship
-end
+    attr_reader :name, :type, :booty
+  
+    @@ships = []
+  
+    def initialize(args)
+      @name = args[:name]
+      @type = args[:type]
+      @booty = args[:booty]
+      @@ships << self
+    end
+  
+    def self.all
+      @@ships
+    end
+  
+    def self.clear
+      @@ships = []
+    end
+  end
